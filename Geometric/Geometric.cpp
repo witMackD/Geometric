8fca266009e454c0f0f739c35c8b835e25090a05
Maciej Drozdowicz <maciej.drozdowicz@wit.edu.pl>
2019-12-08 20:37:35 +0100

    It Works

--- Geometric/Geometric.cpp
+++ Geometric/Geometric.cpp
@@ -3,19 +3,27 @@
 
 #include "pch.h"
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+class Geometric
+{
+public:
+	Geometric(double a0, double  q) : q_(q), a0_(a0) {	 }
+	double operator[](int n) const;
+private:
+	double q_;
+	double a0_;
+};
+
+double Geometric::operator[](int n) const
+{
+	return a0_ * pow(q_, n);
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	const Geometric geometric(1., 0.5); std::cout << geometric[2] << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
